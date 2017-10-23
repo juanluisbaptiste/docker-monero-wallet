73f03a603e5f0648fe3e67e07c498bb5cf48ac54
Juan Luis Baptiste <juan.baptiste@gmail.com>
2017-10-23 00:58:43 -0500

    Add environment variable LOG_LEVEL to control monerod debug level

--- start_monerod.sh
+++ start_monerod.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
-#env
+env
+DEFAULT_LOG_LEVEL=1
+if env | grep -q ^LOG_LEVEL=
+then
+  echo "LOG_LEVEL set to ${LOG_LEVEL}"
+else
+  echo "LOG_LEVEL not set, setting default value (LOG_LEVEL=$DEFAULT_LOG_LEVEL)."
+  LOG_LEVEL=$DEFAULT_LOG_LEVEL
+  export LOG_LEVEL
+fi
 supervisord -c /etc/supervisor/supervisord.conf
-#${HOME}/monero-gui/monerod --check-updates disabled --log-level 4
