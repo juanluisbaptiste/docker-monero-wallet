6e028c5d50b41639fb9e281cee693c1cbc52d493
Juan Luis Baptiste <juan.baptiste@gmail.com>
2017-11-09 21:43:33 -0500

    Added env variable ENABLE_WEB_VIEW to use Xpra to display the wallet on a browser window.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-xpra start --bind-tcp=0.0.0.0:10000 --html=on --start=/home/monero/monero-gui/start-gui.sh --daemon=no --pulseaudio=no --notifications=no --bell=no
-xvfb-run --server-args="-screen 0, 1600x1200x24"
+#Use Xpra to enable access through a web browser
+if [ "${ENABLE_WEB_VIEW}" == "yes" ]; then
+  xpra start --bind-tcp=0.0.0.0:10000 --html=on --start=/home/monero/monero-gui/start-gui.sh --daemon=no --pulseaudio=no --notifications=no --bell=no
+  xvfb-run --server-args="-screen 0, 1600x1200x24"
+else
+  /home/monero/monero-gui/start-gui.sh
+fi
