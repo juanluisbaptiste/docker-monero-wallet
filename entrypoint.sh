53a8aee3494b0e54f1d48ec0d21eef901e074f75
Juan Luis Baptiste <juan.baptiste@gmail.com>
2017-11-09 23:27:42 -0500

    Add environment variable WEB_VIEW_PORT to change the Xpra port and added -displayfd to the xvfb-run command to see if it works on headless servers like Synology NAS

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,8 +7,8 @@ fi
 
 #Use Xpra to enable access through a web browser
 if [ "${ENABLE_WEB_VIEW}" == "yes" ]; then
-  xpra start --bind-tcp=0.0.0.0:10000 --html=on --start=/home/monero/monero-gui/start-gui.sh --daemon=no --pulseaudio=no --notifications=no --bell=no
-  xvfb-run --server-args="-screen 0, 1600x1200x24"
+  xpra start --bind-tcp=0.0.0.0:${WEB_VIEW_PORT} --html=on --start=/home/monero/monero-gui/start-gui.sh --daemon=no --pulseaudio=no --notifications=no --bell=no
+  xvfb-run --server-args="-screen 0, 1600x1200x24 -displayfd ${DISPLAY}"
 else
   /home/monero/monero-gui/start-gui.sh
 fi
