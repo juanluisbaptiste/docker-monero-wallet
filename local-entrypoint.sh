2b51f44405bc8352d24d3b9cc647edb9c4ad7535
Juan Luis Baptiste <juan.baptiste@gmail.com>
2017-11-13 19:12:11 -0500

    Modified local-entrypoint.sh to use base entrypoint.sh

--- local-entrypoint.sh
+++ local-entrypoint.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
+env > .envrc
+echo CMD="/home/monero/monero-gui/start-gui.sh" >> .envrc
 
-if [ "${DEBUG}" == "yes" ]; then
-  env
-  set -x
-fi
-
-#Use Xpra to enable access through a web browser
-if [ "${ENABLE_WEB_VIEW}" == "yes" ]; then
-  xpra start --bind-tcp=0.0.0.0:${WEB_VIEW_PORT} --html=on --start=/home/monero/monero-gui/start-gui.sh --daemon=no --pulseaudio=no --notifications=no --bell=no
-  xvfb-run --server-args="-screen 0, 1600x1200x24 -displayfd ${DISPLAY}"
-else
-  /home/monero/monero-gui/start-gui.sh
-fi
+. /entrypoint.sh
